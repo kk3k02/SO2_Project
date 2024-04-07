7a3ed1cd6401d90a1ee03c7796ef3fd64626c77c
kk3k02 <kubajak@outlook.com>
2024-04-07 19:48:33 +0200

    Random changes of speed

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,5 +1,6 @@
 #include "rectangle.h"
 #include <GLFW/glfw3.h>
+#include <random>
 
 Rectangle::Rectangle(float x, float y, float vx, float width, float height) :
         x(x), y(y), vx(vx), width(width), height(height) {}
@@ -18,5 +19,11 @@ void Rectangle::move(float minX, float maxX) {
     x += vx;
     if (x <= minX || x >= maxX - width) {
         vx *= -1;
+        // Losowanie nowej prędkości w zakresie od -5.0 do 5.0
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> distribution(-5.0f, 5.0f);
+        vx = distribution(gen);
     }
 }
+
